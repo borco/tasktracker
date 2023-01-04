a940c1b790487d2b5680ffdbd5cb847d9e599b32
Ioan Calin Borcoman <iborco@gmail.com>
2023-01-04 11:56:49 +0200

    removed old message handler

--- src/lib/messagehandler.h
+++
@@ -1,7 +0,0 @@
-/*
-    Copyright 2022 by Ioan Calin Borcoman <iborco@gmail.com>
-*/
-
-#include <QString>
-
-void message_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
