4cd766941896742b798e19be8e507acfc3e6e23e
Ioan Calin Borcoman <iborco@gmail.com>
2023-01-14 12:07:01 +0200

    added simple test to loading a task list model from yaml

--- tests/test_tasklistmodel.cpp
+++ tests/test_tasklistmodel.cpp
@@ -10,6 +10,30 @@ class TestTaskListModel: public QObject
     Q_OBJECT
 
 private slots:
+    void test_load_tasks_data() {
+        QTest::addColumn<QString>("data");
+        QTest::addColumn<QStringList>("taskNames");
+
+        QTest::newRow("No tasks field") << R"(
+)" << QStringList();
+
+        QTest::newRow("Empty tasks list") << R"(
+tasks: []
+)" << QStringList();
+    }
+
+    void test_load_tasks() {
+        QFETCH(QString, data);
+        QFETCH(QStringList, taskNames);
+
+        TaskListModel model;
+        model.loadFromData(data.toUtf8());
+        QCOMPARE(model.size(), taskNames.size());
+        for(int i = 0; i < model.size(); ++i) {
+            QCOMPARE(model.get(i)->name(), taskNames[i]);
+        }
+    }
+
 };
 
 QTEST_MAIN(TestTaskListModel)
