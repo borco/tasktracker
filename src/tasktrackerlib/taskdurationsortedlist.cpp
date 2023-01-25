bce496057414014f920c7198636b591b598df297
Ioan Calin Borcoman <iborco@gmail.com>
2023-01-25 12:56:39 +0200

    derive TaskDurationSortedList from DateSortedList

--- src/tasktrackerlib/taskdurationsortedlist.cpp
+++ src/tasktrackerlib/taskdurationsortedlist.cpp
@@ -4,8 +4,6 @@
 
 #include "taskdurationsortedlist.h"
 
-#include "taskduration.h"
-
 #include "yaml-cpp/yaml.h" // IWYU pragma: keep
 
 namespace {
@@ -15,19 +13,13 @@ static const char* DurationsYamlName = "durations";
 namespace tasktrackerlib {
 
 TaskDurationSortedList::TaskDurationSortedList(QObject *parent)
-    : QObject{parent}
+    : DateSortedList<TaskDuration>{parent}
 {
-}
-
-TaskDurationSortedList::~TaskDurationSortedList()
-{
-    qDeleteAll(m_durations);
 }
 
 void TaskDurationSortedList::loadFromYaml(const YAML::Node &node)
 {
-    qDeleteAll(m_durations);
-    m_durations.clear();
+    clear();
 
     if (node.IsNull()) {
         emit sizeChanged();
@@ -52,58 +44,12 @@ void TaskDurationSortedList::loadFromYaml(const YAML::Node &node)
             qCritical().nospace() << DurationsYamlName << "[" << i << "] is not a dictionary";
             continue;
         }
-        auto duration = new TaskDuration();
+        auto duration = new TaskDuration(this);
         duration->loadFromYaml(duration_node);
         insert(duration);
     }
 
     emit sizeChanged();
-}
-
-void TaskDurationSortedList::insert(TaskDuration *duration)
-{
-    if (!duration->parent())
-        duration->setParent(this);
-
-    if (!duration->start().isValid()) {
-        auto now = QDateTime::currentDateTimeUtc();
-
-        duration->setStart(now);
-        duration->setStop(now);
-    }
-
-    auto it = std::upper_bound(
-                m_durations.begin(),
-                m_durations.end(),
-                duration->start(),
-                [](const QDateTime& dateTime, const TaskDuration* duration) {
-        return dateTime < duration->start();
-    });
-    m_durations.insert(it, duration);
-
-    emit sizeChanged();
-}
-
-void TaskDurationSortedList::clear()
-{
-    qDeleteAll(m_durations);
-    m_durations.clear();
-    emit sizeChanged();
-}
-
-int TaskDurationSortedList::size() const
-{
-    return m_durations.size();
-}
-
-TaskDuration *TaskDurationSortedList::operator[](int i)
-{
-    return m_durations[i];
-}
-
-const TaskDuration *TaskDurationSortedList::operator[](int i) const
-{
-    return m_durations[i];
 }
 
 } // namespace tasktrackerlib
--- src/tasktrackerlib/taskdurationsortedlist.h
+++ src/tasktrackerlib/taskdurationsortedlist.h
@@ -4,39 +4,23 @@
 
 #pragma once
 
+#include "datesortedlist.h"
+#include "taskduration.h"
 #include "yamlbackingstore.h"
 
 #include <QQmlEngine>
 
 namespace tasktrackerlib {
 
-class TaskDuration;
-
-class TaskDurationSortedList : public QObject, public YamlBackingStore
+class TaskDurationSortedList : public DateSortedList<TaskDuration>, public YamlBackingStore
 {
     Q_OBJECT
     QML_ELEMENT
 
-    Q_PROPERTY(int size READ size NOTIFY sizeChanged)
-
 public:
     explicit TaskDurationSortedList(QObject *parent = nullptr);
-    ~TaskDurationSortedList() override;
 
     void loadFromYaml(const YAML::Node &node) override;
-
-    void insert(TaskDuration* duration);
-
-    void clear();
-    int size() const;
-    TaskDuration* operator[](int i);
-    const TaskDuration* operator[](int i) const;
-
-signals:
-    void sizeChanged();
-
-private:
-    std::vector<TaskDuration*> m_durations;
 };
 
 } // namespace tasktrackerlib
