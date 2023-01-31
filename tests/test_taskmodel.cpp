78993889a7c2059bf90288f2c771ede05215be7d
Ioan Calin Borcoman <iborco@gmail.com>
2023-01-31 15:46:37 +0200

    updated test

--- tests/test_taskmodel.cpp
+++ tests/test_taskmodel.cpp
@@ -45,24 +45,7 @@ tasks: []
     }
 
     void test_save_to_yaml_some_task() {
-        TaskModel model;
-        model.loadFromData(R"(tasks:
-- name: Foo Bar
-  durations:
-  - start: 2021-02-03T01:02:03Z
-    stop: 2021-02-03T01:02:04Z
-- name: foo bar
-  counts:
-  - date: 2023-01-01
-    count: 10
-  - date: 2023-01-02
-    count: 1
-- name: Foo bar
-- name: foo Bar
-)");
-        YAML::Emitter out;
-        model.saveToYaml(out);
-        QCOMPARE(QString(out.c_str()), R"(tasks:
+        QString data = R"(tasks:
   - name: Foo Bar
     durations:
       - start: 2021-02-03T01:02:03Z
@@ -74,7 +57,13 @@ tasks: []
       - date: 2023-01-02
         count: 1
   - name: Foo bar
-  - name: foo Bar)");
+  - name: foo Bar)";
+
+        TaskModel model;
+        model.loadFromData(data.toUtf8());
+        YAML::Emitter out;
+        model.saveToYaml(out);
+        QCOMPARE(QString(out.c_str()), data);
     }
 };
 
